46041bcbdb01fa8766065906590347e6f881e854
Abdelrahman Khaled <abody.khaled.saad@gmail.com>
2025-12-29 21:54:10 +0200

    setup city_viewmodel

---
+++ app/src/main/java/com/example/weather/CityViewModel.java
@@ -0,0 +1,28 @@
+package com.example.weather;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+public class CityViewModel extends ViewModel {
+    private MutableLiveData<City> selectedCity = new MutableLiveData<>();
+    private MutableLiveData<ArrayList<City>> citiesList = new MutableLiveData<>();
+
+    public LiveData<City> getSelectedCity() {
+        return selectedCity;
+    }
+
+    public LiveData<ArrayList<City>> getCityList() {
+        return citiesList;
+    }
+
+    public void setSelectedCity(City city) {
+        selectedCity.setValue(city);
+    }
+
+    public void setCityList(ArrayList<City> list) {
+        citiesList.setValue(list);
+    }
+}
